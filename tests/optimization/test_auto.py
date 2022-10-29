3a20b2228652ca068a69b546d8f320eacfe4b6f1
Bruno Benicio <brunobenicio2214@gmail.com>
2022-10-29 01:06:17 -0300

    fix tests

--- tests/optimization/test_auto.py
+++ tests/optimization/test_auto.py
@@ -1,16 +1,31 @@
 import pandas as pd
-from myml.optimization.auto import AutoML, AutoPipelineChooser
+from myml.optimization.auto import AutoConfig, AutoML, AutoPipelineChooser, AutoProgressBar
 from myml.optimization.metric import Metric
+from myml.optimization.optimizer import OptimizationConfig
 from myml.utils import ProblemType
 
 def test_autopipelinechooser(data: pd.DataFrame, target: pd.Series):
-    chooser = AutoPipelineChooser(ProblemType.classification, Metric.f1, evaluations=10, n_jobs=-1, seed=24)
+    config = AutoConfig(
+        ProblemType.classification,
+        OptimizationConfig(Metric.f1, evaluations=10, n_jobs=2, seed=24)
+    )
+    chooser = AutoPipelineChooser(config)
+    AutoProgressBar('Auto', chooser)
+    
     chooser.numeric_features = data.columns.to_list()
     _, result = chooser.optimize(data, target)
-    assert result >= 0.96
+    print(result)
+    assert result >= 0.98
 
 def test_automl(data: pd.DataFrame, target: pd.Series):
-    chooser = AutoML(ProblemType.classification, Metric.f1, evaluations=10, n_jobs=-1, seed=24)
-    _, result, test_result = chooser.optimize(data, target, voting=5)
+    config = AutoConfig(
+        ProblemType.classification,
+        OptimizationConfig(Metric.f1, evaluations=10, n_jobs=2, seed=24)
+    )
+    chooser = AutoML(config)
+    AutoProgressBar('Auto', chooser)
+    
+    _, result, test_result = chooser.optimize(data, target)
 
-    assert result >= 0.97 and test_result >= 0.96
+    print(result, test_result)
+    assert result >= 0.97 and test_result >= 0.97
