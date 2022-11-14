63555ba8c2ea6adfca27dc97ac7ad902a45dd61c
Bruno Benicio <brunobenicio2214@gmail.com>
2022-11-13 21:31:36 -0300

    increment classifier search space

--- myml/models/_classification.py
+++ myml/models/_classification.py
@@ -6,18 +6,30 @@ with scikit-learn compatible classifiers and their respective hyperparameter sea
 """
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from skopt.space import Categorical, Integer, Real
 from myml.optimization.search import HyperparameterSearchSpace, ModelSearchSpace
+from myml.utils import get_mlp_hidden_layer_sizes
 
 
 estimators = ModelSearchSpace()
 """ Search space for classifiers. """
 
 estimators[LogisticRegression()] = HyperparameterSearchSpace(
-    penalty=Categorical(['none', 'l2']),
-    max_iter=Integer(100, 1000)
+    penalty=Categorical(['none', 'l1', 'l2']),
+    solver=Categorical(['saga']),
+    max_iter=Integer(100, 100000, 'log-uniform')
+)
+
+estimators[GaussianNB()] = HyperparameterSearchSpace(
+    priors=Categorical([None])
+)
+
+estimators[MultinomialNB()] = HyperparameterSearchSpace(
+    fit_prior=Categorical([False, True])
 )
 
 estimators[RandomForestClassifier()] = HyperparameterSearchSpace(
@@ -39,6 +51,14 @@ estimators[AdaBoostClassifier()] = HyperparameterSearchSpace(
 )
 
 estimators[KNeighborsClassifier()] = HyperparameterSearchSpace(
-    n_neighbors=Integer(3, 50),
-    weights=Categorical(['uniform', 'distance'])
+    n_neighbors=Integer(3, 100),
+    weights=Categorical(['uniform', 'distance']),
+    p=Categorical([1, 2])
+)
+
+estimators[MLPClassifier()] = HyperparameterSearchSpace(
+    hidden_layer_sizes=Categorical(get_mlp_hidden_layer_sizes([1,2,3], [16,32,64,128,256])),
+    activation=Categorical(['relu', 'tanh']),
+    alpha=Real(1e-6, 1e-1, 'log-uniform'),
+    learning_rate_init=Real(1e-4, 1e-1, 'log-uniform')
 )
--- myml/utils.py
+++ myml/utils.py
@@ -1,7 +1,7 @@
 """ Utilities for on the rest of the package. """
 
 from enum import IntEnum
-from typing import List
+from typing import List, Iterable, Tuple
 import numpy as np
 import pandas as pd
 
@@ -85,3 +85,24 @@ def filter_by_types(X: Features, types: List[str]) -> pd.DataFrame:
         df = X
 
     return df.select_dtypes(include=types)
+
+def get_mlp_hidden_layer_sizes(layer_counts: Iterable[int], nodes_counts: Iterable[int]) -> List[Tuple]:
+    """
+    Get MLP hidden layer sizes hyperparameter from layer and node counts.
+
+    Create tuples for each node count on different layering
+    configurations e.g. `[(16,), (16,16), (32,), (32,32)]`.
+    
+    Parameters
+    ----------
+    layer_counts : Iterable of int
+        Possible layer counts to generate.
+    nodes_counts : Iterable of int
+        Possible node counts to generate.
+
+    Returns
+    -------
+    list of tuple
+        Hidden layer sizes for the MLP estimators.
+    """
+    return [tuple(nodes for _ in range(layers)) for layers in layer_counts for nodes in nodes_counts]
