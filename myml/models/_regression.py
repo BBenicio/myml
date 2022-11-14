c352024f77562d0bf208642a602fe586170ddc44
Bruno Benicio <brunobenicio2214@gmail.com>
2022-11-13 22:00:03 -0300

    increment regression search space

--- myml/models/_regression.py
+++ myml/models/_regression.py
@@ -5,19 +5,26 @@ The `estimators` object contains the search space for regression problems,
 with scikit-learn compatible regressors and their respective hyperparameter search spaces.
 """
 
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet, LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
 from skopt.space import Categorical, Integer, Real
 from myml.optimization.search import HyperparameterSearchSpace, ModelSearchSpace
+from myml.utils import get_mlp_hidden_layer_sizes
 
 
 estimators = ModelSearchSpace()
 """ Search space for regressors. """
 
-estimators[Ridge()] = HyperparameterSearchSpace(
-    alpha=Real(0, 2),
-    max_iter=Integer(100, 1000)
+estimators[LinearRegression()] = HyperparameterSearchSpace(
+    fit_intercept=Categorical([True])
+)
+
+estimators[ElasticNet()] = HyperparameterSearchSpace(
+    alpha=Real(0, 10),
+    l1_ration=Real(0, 1),
+    max_iter=Integer(100, 100000, 'log-uniform')
 )
 
 estimators[RandomForestRegressor()] = HyperparameterSearchSpace(
@@ -39,6 +46,14 @@ estimators[AdaBoostRegressor()] = HyperparameterSearchSpace(
 )
 
 estimators[KNeighborsRegressor()] = HyperparameterSearchSpace(
-    n_neighbors=Integer(3, 50),
-    weights=Categorical(['uniform', 'distance'])
+    n_neighbors=Integer(3, 100),
+    weights=Categorical(['uniform', 'distance']),
+    p=Categorical([1, 2])
+)
+
+estimators[MLPRegressor()] = HyperparameterSearchSpace(
+    hidden_layer_sizes=Categorical(get_mlp_hidden_layer_sizes([1,2,3], [16,32,64,128,256])),
+    activation=Categorical(['relu', 'tanh']),
+    alpha=Real(1e-6, 1e-1, 'log-uniform'),
+    learning_rate_init=Real(1e-4, 1e-1, 'log-uniform')
 )
